78196eff8c177f4e9a86a1403170d8a472218f2c
yifanlan <1522254605@qq.com>
2020-08-20 20:57:43 +0800

    L9 2

---
+++ src/L9_MaxDoubleSliceSum/Solution.java
@@ -0,0 +1,75 @@
+package L9_MaxDoubleSliceSum;
+
+/**
+ * A non-empty array A consisting of N integers is given.
+ *
+ * A triplet (X, Y, Z), such that 0 ≤ X < Y < Z < N, is called a double slice.
+ *
+ * The sum of double slice (X, Y, Z) is the total of A[X + 1] + A[X + 2] + ... +
+ * A[Y − 1] + A[Y + 1] + A[Y + 2] + ... + A[Z − 1].
+ *
+ * For example, array A such that:
+ *
+ *     A[0] = 3
+ *     A[1] = 2
+ *     A[2] = 6
+ *     A[3] = -1
+ *     A[4] = 4
+ *     A[5] = 5
+ *     A[6] = -1
+ *     A[7] = 2
+ * contains the following example double slices:
+ *
+ * double slice (0, 3, 6), sum is 2 + 6 + 4 + 5 = 17,
+ * double slice (0, 3, 7), sum is 2 + 6 + 4 + 5 − 1 = 16,
+ * double slice (3, 4, 5), sum is 0.
+ * The goal is to find the maximal sum of any double slice.
+ *
+ * Write a function:
+ *
+ * class Solution { public int solution(int[] A); }
+ *
+ * that, given a non-empty array A consisting of N integers, returns the maximal sum of any double slice.
+ *
+ * For example, given:
+ *
+ *     A[0] = 3
+ *     A[1] = 2
+ *     A[2] = 6
+ *     A[3] = -1
+ *     A[4] = 4
+ *     A[5] = 5
+ *     A[6] = -1
+ *     A[7] = 2
+ * the function should return 17, because no double slice of array A has a sum of greater than 17.
+ *
+ * Write an efficient algorithm for the following assumptions:
+ *
+ * N is an integer within the range [3..100,000];
+ * each element of array A is an integer within the range [−10,000..10,000].
+ */
+public class Solution {
+    public int solution(int[] A) {
+        // write your code in Java SE 8
+        int []max_sum_forward = new int[A.length];
+        int []max_sum_backward = new int[A.length];
+        int max_sum_here = 0;
+        int max_sum_present = Integer.MIN_VALUE;
+        max_sum_forward[0] = 0;
+        max_sum_backward[A.length - 1] = 0;
+        if (A.length <= 3){
+            return 0;
+        }
+        for (int i = 1; i < A.length; i++){
+            max_sum_forward[i] = Integer.max(max_sum_forward[i - 1] + A[i], A[i]);
+        }
+        for (int i = A.length - 2; i >= 0; i--){
+            max_sum_backward[i] = Integer.max(max_sum_backward[i + 1] + A[i], A[i]);
+        }
+        for (int i = 1; i < A.length - 1; i++){
+            max_sum_here = Integer.max(max_sum_forward[i - 1], 0) + Integer.max(max_sum_backward[i + 1],0);
+            max_sum_present = Integer.max(max_sum_present, max_sum_here);
+        }
+        return max_sum_present;
+    }
+}
